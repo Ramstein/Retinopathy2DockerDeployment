1443c2ef01b25d4d0103c241aac1dfd0125f21b4
Ramstein <gerialworld@gmail.com>
2021-01-07 12:32:09 +0530

    code changes made by Ramstein

--- container/build_and_push.sh
+++ container/build_and_push.sh
@@ -7,35 +7,30 @@
 # machine and combined with the account and region to form the repository name for ECR.
 image=$1
 
-if [ "$image" == "" ]
-then
-    echo "Usage: $0 <image-name>"
-    exit 1
+if [ "$image" == "" ]; then
+  echo "Usage: $0 <image-name>"
+  exit 1
 fi
 
 # Get the account number associated with the current IAM credentials
 account=$(aws sts get-caller-identity --query Account --output text)
 
-if [ $? -ne 0 ]
-then
-    exit 255
+if [ $? -ne 0 ]; then
+  exit 255
 fi
-
 
 # Get the region defined in the current configuration (default to us-west-2 if none defined)
 region=$(aws configure get region)
 region=${region:-eu-west-1}
 
-
 fullname="${account}.dkr.ecr.${region}.amazonaws.com/${image}:latest"
 
 # If the repository doesn't exist in ECR, create it.
 
-aws ecr describe-repositories --repository-names "${image}" > /dev/null 2>&1
+aws ecr describe-repositories --repository-names "${image}" >/dev/null 2>&1
 
-if [ $? -ne 0 ]
-then
-    aws ecr create-repository --repository-name "${image}" > /dev/null
+if [ $? -ne 0 ]; then
+  aws ecr create-repository --repository-name "${image}" >/dev/null
 fi
 
 # Get the login command from ECR and execute it directly
